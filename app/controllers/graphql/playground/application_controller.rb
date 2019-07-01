12627c563a930c8e0976a3bbb550f0100ee12963
Pan Jie <panjie@growingio.com>
2019-07-01 13:25:46 +0800

    Remove helper from application controller.

--- app/controllers/graphql/playground/application_controller.rb
+++ app/controllers/graphql/playground/application_controller.rb
@@ -2,7 +2,6 @@ module GraphQL
   module Playground
     class ApplicationController < ActionController::Base
       protect_from_forgery with: :exception
-      helper GraphQL::Playground::ApplicationHelper
     end
   end
 end
