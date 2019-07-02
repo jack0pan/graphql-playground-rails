2e7337c9c461ced84d705699936f0e03b82a3a05
Pan Jie <panjie@growingio.com>
2019-07-02 11:04:59 +0800

    Set layout in application controller.

--- app/controllers/graphql/playground/application_controller.rb
+++ app/controllers/graphql/playground/application_controller.rb
@@ -2,6 +2,7 @@ module GraphQL
   module Playground
     class ApplicationController < ActionController::Base
       protect_from_forgery with: :exception
+      layout "graphql/playground/application"
     end
   end
 end
